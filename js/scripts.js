37171d1fe25ab1c9a77bd813aefb5e72be466638
rehemafaith <rehemafaith01@gmail.com>
2019-02-25 11:23:34 +0300

    add new tags'

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+// Business logic
 var add=function(question1,question2,question3,question4,question5){
  return question1+question2+question3+question4+question5
 }
+// UI logic
 $(document).ready(function(){
  $("button#submit").click(function(){
  var question1=parseInt($("input:radio[name=answer1]:checked").val());
@@ -9,12 +11,12 @@ $(document).ready(function(){
  var question4=parseInt($("input:radio[name=answer4]:checked").val());
  var question5=parseInt($("input:radio[name=answer5]:checked").val());
  var score=add(question1,question2,question3,question4,question5)
+ $(".output").show();
+ $(".card").hide();
+ $("#gifer").show();
+ $(".output").append("Your score is:" + score );
+ event.preventDefault();
+
 
  })
 });
- // UI logic
-
- $(document).ready(function(event){
-   $("#output").text("Your score is:" + score);
-   event.preventDefault();
- });
