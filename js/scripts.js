e488231cbe485433ac64f6f09af1cfd9dd5396cf
rehemafaith <rehemafaith01@gmail.com>
2019-02-25 08:34:41 +0300

    add javascript and jQuery codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,20 @@
-var question1 = $("input:radio[name:answer1]:checked").val();
-var question2 = $("input:radio[name:answer2]:checked").val();
-var question3 = $("input:radio[name:answer3]:checked").val();
-var question4 = $("input:radio[name:answer4]:checked").val();
-var question5 = $("input:radio[name:answer5]:checked").val();
-var score = 0
-
-if (question1 == "1995"){
-  return score++
-};
-if (question2 == "parseInt"){
-  return score++
-};
-if (question3 == "module"){
-  return score++
-};
-if (question4 == "yes"){
-  return score++
-};
-if (question == "Hyper"){
-  return score++
+var add=function(question1,question2,question3,question4,question5){
+ return question1+question2+question3+question4+question5
 }
+$(document).ready(function(){
+ $("button#submit").click(function(){
+ var question1=parseInt($("input:radio[name=answer1]:checked").val());
+ var question2=parseInt($("input:radio[name=answer2]:checked").val());
+ var question3=parseInt($("input:radio[name=answer3]:checked").val());
+ var question4=parseInt($("input:radio[name=answer4]:checked").val());
+ var question5=parseInt($("input:radio[name=answer5]:checked").val());
+ var score=add(question1,question2,question3,question4,question5)
 
-
-
-// user interface logic
-$(document).ready(function(event){
-  $("#questions").submit(function(event){
-    var result = score
-  });
+ })
 });
+ // UI logic
+
+ $(document).ready(function(event){
+   $("#output").text("Your score is:" + score);
+   event.preventDefault();
+ });
